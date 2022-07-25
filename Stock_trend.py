ab4726cc48688be2f462b1b0216a88eaafaacea4
Pratik  Datey <100415960+pratikdatey@users.noreply.github.com>
2022-07-25 10:38:53 +0530

    Update Stock_trend.py

--- Stock_trend.py
+++ Stock_trend.py
@@ -154,6 +154,6 @@ tail=data_testing.tail(80)
 ms_tail=scaler.fit_transform(tail)
 test_pred=(ms_tail).reshape([1,80])
 
-pred_value=int(model.predict(test_pred) * (tail_max[0))
+pred_value=int(model.predict(test_pred) * (tail_max[0]))
 st.write ('Predicted price of {} on {} date will be {}.'.format('user_input',date.today(),pred_value))
 
